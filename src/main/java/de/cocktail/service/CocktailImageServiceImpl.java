0b39853d6e2de760c31b27cb44b08368b5602aff
Black82 <blackrailean@gmail.com>
2019-04-07 17:03:47 +0200

    controls Whether The Fail You Receive Is Image

--- src/main/java/de/cocktail/service/CocktailImageServiceImpl.java
+++ src/main/java/de/cocktail/service/CocktailImageServiceImpl.java
@@ -31,7 +31,7 @@ import java.util.Optional;
 @Service
 public class CocktailImageServiceImpl implements CocktailImageService{
 
-    private final CocktailRepository COKTAIL_REPOSITORY;
+    private final CocktailRepository COCKTAIL_REPOSITORY;
 
         private final Path FILE_STORAGE_LOCATION =Paths
                  .get("src\\main\\resources\\image")
@@ -39,8 +39,8 @@ public class CocktailImageServiceImpl implements CocktailImageService{
                  .normalize();
 
     @Autowired
-    public CocktailImageServiceImpl(CocktailRepository COKTAIL_REPOSITORY) {
-        this.COKTAIL_REPOSITORY = COKTAIL_REPOSITORY;
+    public CocktailImageServiceImpl(CocktailRepository COCKTAIL_REPOSITORY) {
+        this.COCKTAIL_REPOSITORY = COCKTAIL_REPOSITORY;
     }
     public String composeFileDownloadUriAndSavetImage(Long id, MultipartFile file) {
         String fileName = composesTheNameofTheImage(file, id);
@@ -81,13 +81,13 @@ public class CocktailImageServiceImpl implements CocktailImageService{
             }
         }
         private void setImageToCocktail(Long id, String uri, String fileName,String alt) {
-            Optional <Cocktail> byId = COKTAIL_REPOSITORY.findById(id);
+            Optional <Cocktail> byId = COCKTAIL_REPOSITORY.findById(id);
             if (byId.isPresent()) {
                 Cocktail cocktail = byId.get();
                 cocktail.getImage().setPatch(uri);
                 cocktail.getImage().setTitle(fileName);
                 cocktail.getImage().setAlt(alt);
-                COKTAIL_REPOSITORY.save(cocktail);
+                COCKTAIL_REPOSITORY.save(cocktail);
             }
             else throw new NotFoundCocktail("This cocktail does not exist");
 
